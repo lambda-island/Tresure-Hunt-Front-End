c9beca6901f758fe2ff1719ff99d61777117b5d1
Yanrong Xu <xuyanrong@icloud.com>
2019-07-29 17:28:27 -0400

    react app able to send a inital get request to service

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, {Component} from 'react';
 import './App.css';
+import secToken from '../src/Components/tokens/Token'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    let config = {
+      headers: {Authorization: secToken}
+    }
+    axios
+      .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', config)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   render() {
     return (
       <div className='App'>
